51d3510e4380ab22c68857c1a82bea74e2adb1ae
DarioPazmino <35546398+DarioPazmino@users.noreply.github.com>
2024-09-17 16:02:45 -0500

    autocomplete component with universities API

---
+++ src/components/Autocomplete.jsx
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { useState, useMemo } from "react";
+import { debounce } from "lodash";
+import './Autocomplete.css';
+
+const url = 'http://universities.hipolabs.com/search';
+
+const Autocomplete = function () {
+  const [query, setQuery] = useState('');
+  const [selected, setSelected] = useState({});
+  const [suggestions, setSuggestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showList, setShowList] = useState(false);
+
+  const fetchData = async (q) => {
+    setIsLoading(true);
+    await axios.get(url, {
+      params: {
+        name: q,
+      }
+    })
+      .then(response => {
+        setSuggestions(response.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setSuggestions([]);
+        setIsLoading(false);
+        console.log(error);
+      })
+  };
+
+  const debounced = useMemo(() => debounce(fetchData, 500), []);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+    debounced(value);
+    setShowList(true);
+  };
+
+  const handleSelect = (option) => {
+    setQuery(option.name);
+    setSelected(option);
+    setShowList(false);
+  }
+
+  return (
+    <div className="container">
+      <label htmlFor="search-bar">Find a University:</label>
+      <input
+        name="search-bar"
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Start typing to search universities"
+      />
+      <div className="list">
+        {(!isLoading && suggestions.length > 0 && showList) && suggestions?.map((suggestion) => (
+          <div
+            key={suggestion.name}
+            onClick={() => handleSelect(suggestion)}
+            className={query === suggestion.name ? 'selected' : ''}
+          >
+            {suggestion.name}
+          </div>
+        ))}
+      </div>
+      {Object.keys(selected).length > 0 && (
+        <div className="selected-container">
+          <h3>Name</h3>
+          <span>{selected.name}</span>
+          <h3>Country</h3>
+          <span>{selected.country}</span>
+          <h3>Domains</h3>
+          {selected.domains.map(domain => (
+            <span>{domain}</span>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Autocomplete;
